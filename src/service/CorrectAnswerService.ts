db718b071a300590fd055d81210ab22b8bcf177c
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-05-21 16:04:40 +0100

    refactor

--- src/service/CorrectAnswerService.ts
+++ src/service/CorrectAnswerService.ts
@@ -16,19 +16,15 @@ export default class CorrectAnswerService{
         if(!dbResponse) throw new Error("User does not exist");
         const age = dbResponse.age;
 
-        const prompt = `
-You are Math King Ade — a wise Nigerian math tutor who uses .
-A student named "${checkAnswerRequest.username}" aged ${age} is asking this math question: "${checkAnswerRequest.question}"
-They answered: "${checkAnswerRequest.answer}"
 
-Check if the answer is correct.
-Respond only with:
-- 'true' or 'false' as the first word (always in English)
-- If true: Compliment them by name in English."
-- If false: Explain how to solve it correctly in ${dbResponse.language}, using Lagos markets, farming, or everyday life examples.
-Do NOT repeat the question or answer.
-Make sure that only the first word is in English (true/false), and the rest of the response is entirely in ${dbResponse.language}.
+    const prompt = `
+Solve this math question and return only the final numerical answer.
+Do not include any explanation, words, or markdown.
+Question: "${checkAnswerRequest.question}"
+Answer:
 `.trim();
+
+        let geminiResult = "";
 
         try {
       const response = await axios.post(
@@ -50,13 +46,18 @@ Make sure that only the first word is in English (true/false), and the rest of t
           }
         }
       );
-
-      return response.data.candidates[0].content.parts[0].text.trim();
+     geminiResult = response.data.candidates[0].content.parts[0].text.trim();
     } catch (error: any) {
       console.error('Error calling Gemini:', error.response?.data || error.message);
       throw new Error('Failed to get question from AI');
     }
+
+    if(geminiResult===checkAnswerRequest.answer) return `${checkAnswerRequest.username} you are a genius`;
+    else return `${checkAnswerRequest.username} you failed`;
+
   }
+
+
        
     
 

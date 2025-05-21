66348b9b21ad5bec25f10ca3f28c2176ed630547
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-05-21 19:18:14 +0100

    adjustment

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,36 @@
+// import { DataSource } from 'typeorm';
+// import { User } from './data/model/User';
+// import dotenv from 'dotenv'
+
+// dotenv.config();
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: process.env.DB_HOST,
+//   port: 5432,
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.NAME,
+//   synchronize: true,
+//   dropSchema: true,
+//   logging: false,
+//   entities: [User],
+// });
+
+
 import { DataSource } from 'typeorm';
 import { User } from './data/model/User';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config();
+
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.NAME,
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
   synchronize: true,
   dropSchema: true,
   logging: false,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
   entities: [User],
 });
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,12 @@ AppDataSource.initialize()
   .then(() => {
     console.log('Connected to PostgreSQL');
 
+    const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
     app.get('/', (req, res) => {
       res.send('Welcome to MathMaster Backend!');
     });
